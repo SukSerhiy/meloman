e373b62cb892d4f28afb68bbdd465ff6f44604e6
SukSerhiy <sukserhiy@gmail.com>
2020-12-22 16:41:44 +0200

    removed excess import

--- src/components/pages/Artist/index.js
+++ src/components/pages/Artist/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams, useHistory, Link } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles'
 import { Button } from '@material-ui/core'
 import Slider from 'react-slick'
