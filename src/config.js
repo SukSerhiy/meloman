39aef29c0d622f22377ce17c1698220f387c9737
SukSerhiy <sukserhiy@gmail.com>
2020-12-22 18:18:16 +0200

    Changed env variables names

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ import { store } from '.'
 let api
 
 const createApi = () => {
-  const apiHost = process.env.REACT_APP_API_HOST
-  const authApiHost = process.env.REACT_APP_API_AUTH_HOST
+  const apiHost = process.env.API_HOST
+  const authApiHost = process.env.API_AUTH_HOST
   const { accessToken } = store.getState().auth
-  const refreshToken = process.env.REACT_APP_REFRESH_TOKEN
-  const clientKey = process.env.REACT_APP_CLIENT_KEY
+  const refreshToken = process.env.REFRESH_TOKEN
+  const clientKey = process.env.CLIENT_KEY
 
   return new Api({
     apiHost,
